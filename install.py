306c6603ac0d118d15d8953ee7e7e46bfe89ef7f
ZRGX <139933536+RGX650@users.noreply.github.com>
2024-01-13 07:23:25 +0100

    Update install.py

--- install.py
+++ install.py
@@ -1,5 +1,10 @@
 import os
 import launch
+import git
+
+# Determine the path to the current script file
+script_path = os.path.realpath(__file__)
+script_directory = os.path.dirname(script_path)
 
 # Download the ZIP file containing the tar.gz file
 zip_file_url = "https://github.com/wkpark/bitsandbytes/actions/runs/7117279173/artifacts/1096989694"
@@ -9,23 +14,30 @@ launch.run(f'curl -LJO {zip_file_url} -o {zip_file_name}', "Downloading ZIP file
 # Extract the ZIP file
 launch.run(f'unzip {zip_file_name}', "Extracting ZIP file")
 
-# Navigate to the extracted folder
+# Determine the name of the extracted folder
 extracted_folder = f'{os.path.splitext(zip_file_name)[0]}'
-os.chdir(extracted_folder)
+
+# Navigate to the extracted folder
+os.chdir(os.path.join(script_directory, extracted_folder))
 
 # Extract the tar.gz file
 tar_file_name = "bitsandbytes-0.41.2.tar.gz"
 launch.run(f'tar -xzvf {tar_file_name}', "Extracting tar.gz file")
 
-# Navigate to the extracted folder
+# Determine the name of the extracted tar.gz folder
 extracted_tar_folder = f'{os.path.splitext(tar_file_name)[0]}'
-os.chdir(extracted_tar_folder)
+
+# Navigate to the extracted tar.gz folder
+os.chdir(os.path.join(script_directory, extracted_tar_folder))
 
 # Install the package using pip
-launch.run_pip("install .", "Installing bitsandbytes package")
+launch.run_pip("install .", "Installing bitsandbytes package --prefer-binary")
+
+# Navigate back to the script directory
+os.chdir(script_directory)
 
 # Get the path to the requirements.txt file
-req_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "requirements.txt")
+req_file = os.path.join(script_directory, "requirements.txt")
 
 # Open the requirements.txt file and process each line
 with open(req_file) as file:
@@ -35,7 +47,7 @@ with open(req_file) as file:
         # Check if the package is from a GitHub repository
         if lib.startswith("git+"):
             # Install the GitHub repository package
-            launch.run_pip(f"install {lib}", f"clear object requirement: {lib}")
+            launch.run_pip(f'install {lib}', f"clear object requirement: {lib}")
         elif not launch.is_installed(lib):
             # Install other packages from PyPI if not already installed
-            launch.run_pip(f"install {lib}", f"clear object requirement: {lib}")
+            launch.run_pip(f'install {lib}', f"clear object requirement: {lib}")
