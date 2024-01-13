7621b0cd3273cc781f78e0f8ec6de21d851d0a10
ZRGX <139933536+RGX650@users.noreply.github.com>
2024-01-13 08:37:29 +0100

    Update install.py

--- install.py
+++ install.py
@@ -37,7 +37,7 @@ launch.run_pip("install .", "Installing bitsandbytes package --prefer-binary")
 os.chdir(script_directory)
 
 # Get the path to the requirements.txt file
-req_file = os.path.join(script_directory, "requirements.txt")
+req_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "requirements.txt")
 
 # Open the requirements.txt file and process each line
 with open(req_file) as file:
@@ -47,7 +47,14 @@ with open(req_file) as file:
         # Check if the package is from a GitHub repository
         if lib.startswith("git+"):
             # Install the GitHub repository package
-            launch.run_pip(f'install {lib}', f"clear object requirement: {lib}")
+            git_repo_url = lib[len("git+"):]
+            repo_name = os.path.basename(git_repo_url.rstrip(".git"))
+            install_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), repo_name)
+            
+            if not os.path.exists(install_path):
+                git.Repo.clone_from(git_repo_url, install_path)
+
+            launch.run_pip(f'install -e {install_path}', f"clear object requirement: {lib}")
         elif not launch.is_installed(lib):
             # Install other packages from PyPI if not already installed
             launch.run_pip(f'install {lib}', f"clear object requirement: {lib}")
